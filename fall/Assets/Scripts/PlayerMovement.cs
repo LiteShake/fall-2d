36bdca8ce6b511e1e27e3663690e6cdf97f01f03
LiteShake <strawberryshakelite@gmail.com>
2023-03-02 23:54:33 +0530

    Added movement

--- fall/Assets/Scripts/PlayerMovement.cs
+++ fall/Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is called before the 
+    float speed = 10.0f; // first frame update
+
     void Start()
     {
         
@@ -13,6 +15,14 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        dir.x = -Input.acceleration.y;
+        dir.z = Input.acceleration.x;
+
+        // clamp acceleration vector to unit sphere
+        if (dir.sqrMagnitude > 1)
+            dir.Normalize();
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        dir *= Time.deltaTime;
     }
 }
