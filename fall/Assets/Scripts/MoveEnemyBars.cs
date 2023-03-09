959018c10a3789b3fc83082ef98fbc6ca6841d45
LiteShake <strawberryshakelite@gmail.com>
2023-03-09 18:56:58 +0530

    enemy script #1

--- fall/Assets/Scripts/MoveEnemyBars.cs
+++ fall/Assets/Scripts/MoveEnemyBars.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class MoveEnemyBars : MonoBehaviour
 {
+
+    Rigidbody2D enemy;
+    float speed = 6.9f;
+
+    float startRange = -3.0f;
+    float endRange = 0.69f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemy = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // move from -3 to 0.69
     }
 }
